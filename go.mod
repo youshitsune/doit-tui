5111630f349225d6a24b03d2ca58519812472351
youshitsune <youshitsune@dmz.rs>
2024-08-18 15:48:24 +0200

    feat: printing raw response from list

---
+++ go.mod
@@ -0,0 +1,27 @@
+module main
+
+go 1.23.0
+
+require (
+	github.com/go-zoox/fetch v1.8.2
+	github.com/knadh/koanf/parsers/yaml v0.1.0
+	github.com/knadh/koanf/providers/file v1.1.0
+	github.com/knadh/koanf/v2 v2.1.1
+)
+
+require (
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/go-viper/mapstructure/v2 v2.0.0-alpha.1 // indirect
+	github.com/go-zoox/core-utils v1.2.11 // indirect
+	github.com/go-zoox/headers v1.0.6 // indirect
+	github.com/knadh/koanf/maps v0.1.1 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+	github.com/tidwall/gjson v1.14.4 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path"
+
+	"github.com/go-zoox/fetch"
+	"github.com/knadh/koanf/parsers/yaml"
+	"github.com/knadh/koanf/providers/file"
+	"github.com/knadh/koanf/v2"
+)
+
+var conf = koanf.New(".")
+
+type config struct {
+	url      string
+	username string
+	password string
+}
+
+func load_config() config {
+	config_path := path.Join(os.ExpandEnv("$XDG_CONFIG_HOME"), "/doit/config.yaml")
+	if err := conf.Load(file.Provider(config_path), yaml.Parser()); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	cfg := config{}
+	cfg.url = conf.String("protocol") + "://" + conf.String("url") + ":" + conf.String("port")
+	cfg.password = conf.String("password")
+	cfg.username = conf.String("username")
+	return cfg
+}
+
+func list(cfg config) string {
+	res, err := fetch.Post(cfg.url+"/list", &fetch.Config{
+		Query: map[string]string{
+			"user":     cfg.username,
+			"password": cfg.password,
+		},
+	})
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	return string(res.Body)
+}
+
+func main() {
+	cfg := load_config()
+	fmt.Println(list(cfg))
+}
