52c1013794aecbc6d1a3103326cb10a5ccaec968
youshitsune <youshitsune@dmz.rs>
2024-08-18 17:45:20 +0200

    feat: implement all basic features

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module main
 go 1.23.0
 
 require (
+	github.com/charmbracelet/bubbles v0.18.0
+	github.com/charmbracelet/bubbletea v0.27.0
 	github.com/go-zoox/fetch v1.8.2
 	github.com/knadh/koanf/parsers/yaml v0.1.0
 	github.com/knadh/koanf/providers/file v1.1.0
@@ -10,18 +12,38 @@ require (
 )
 
 require (
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/charmbracelet/lipgloss v0.9.1 // indirect
+	github.com/charmbracelet/x/ansi v0.1.4 // indirect
+	github.com/charmbracelet/x/input v0.1.0 // indirect
+	github.com/charmbracelet/x/term v0.1.1 // indirect
+	github.com/charmbracelet/x/windows v0.1.0 // indirect
+	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/go-viper/mapstructure/v2 v2.0.0-alpha.1 // indirect
 	github.com/go-zoox/core-utils v1.2.11 // indirect
 	github.com/go-zoox/headers v1.0.6 // indirect
 	github.com/knadh/koanf/maps v0.1.1 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
 	github.com/tidwall/gjson v1.14.4 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.1 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	golang.org/x/net v0.17.0 // indirect
-	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"os"
 	"path"
+	"strings"
 
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/go-zoox/fetch"
 	"github.com/knadh/koanf/parsers/yaml"
 	"github.com/knadh/koanf/providers/file"
@@ -34,7 +37,7 @@ func load_config() config {
 	return cfg
 }
 
-func list() string {
+func list() [][]string {
 	res, err := fetch.Post(cfg.url+"/list", &fetch.Config{
 		Query: map[string]string{
 			"user":     cfg.username,
@@ -46,8 +49,16 @@ func list() string {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+	r := make([][]string, 0)
+	t := strings.Split(string(res.Body), "\n")
+	for i := range t {
+		tmp := strings.Split(t[i], "``")
+		if len(tmp) == 3 {
+			r = append(r, tmp)
+		}
+	}
 
-	return string(res.Body)
+	return r
 }
 
 func add(task string) bool {
@@ -92,7 +103,7 @@ func done(id string) bool {
 	}
 }
 
-func delete(id string) bool {
+func deleteTask(id string) bool {
 	res, err := fetch.Post(cfg.url+"/delete", &fetch.Config{
 		Query: map[string]string{
 			"user":     cfg.username,
@@ -113,6 +124,174 @@ func delete(id string) bool {
 	}
 }
 
+type model struct {
+	tasks    [][]string
+	cursor   int
+	selected map[int]struct{}
+	update   bool
+	input    textinput.Model
+	mode     int // STATES: 0 - change of state; 1 - delete tasks; 2 - add tasks
+}
+
+func initialModel() model {
+	input := textinput.New()
+	input.Focus()
+	input.CharLimit = 256
+	input.Width = 20
+	return model{
+		tasks:    list(),
+		selected: make(map[int]struct{}),
+		mode:     0,
+		update:   false,
+		input:    input,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return textinput.Blink
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		if m.mode == 0 {
+			switch msg.String() {
+			case "up", "k":
+				if m.cursor > 0 {
+					m.cursor--
+				}
+			case "down", "j":
+				if m.cursor < len(m.tasks)-1 {
+					m.cursor++
+				}
+			case "y":
+				m.update = true
+			case "enter", " ":
+				_, ok := m.selected[m.cursor]
+				if ok {
+					delete(m.selected, m.cursor)
+				} else {
+					m.selected[m.cursor] = struct{}{}
+				}
+			}
+		} else if m.mode == 1 {
+			var cmd tea.Cmd
+			switch msg.String() {
+			case "enter":
+				m.update = true
+			case "ctrl+c", "q":
+				return m, tea.Quit
+			}
+
+			m.input, cmd = m.input.Update(msg)
+			return m, cmd
+		} else {
+			switch msg.String() {
+			case "up", "k":
+				if m.cursor > 0 {
+					m.cursor--
+				}
+			case "down", "j":
+				if m.cursor < len(m.tasks)-1 {
+					m.cursor++
+				}
+			case "y":
+				m.update = true
+			case "enter", " ":
+				_, ok := m.selected[m.cursor]
+				if ok {
+					delete(m.selected, m.cursor)
+				} else {
+					m.selected[m.cursor] = struct{}{}
+				}
+			}
+		}
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "c":
+			m.mode = 0
+		case "a":
+			m.mode = 1
+		case "d":
+			m.mode = 2
+		}
+
+	}
+
+	if m.update {
+		switch m.mode {
+		case 0:
+			for i := range m.tasks {
+				if _, ok := m.selected[i]; ok {
+					done(m.tasks[i][0])
+					delete(m.selected, i)
+				}
+			}
+			m.tasks = list()
+		case 1:
+			add(m.input.Value())
+			m.input.SetValue("")
+		case 2:
+			for i := range m.tasks {
+				if _, ok := m.selected[i]; ok {
+					deleteTask(m.tasks[i][0])
+					delete(m.selected, i)
+				}
+			}
+		}
+		m.update = false
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	if m.mode == 0 {
+		s := "Tasks:\n\n"
+
+		for i := range m.tasks {
+			cursor := " "
+			if m.cursor == i {
+				cursor = ">"
+			}
+
+			checked := " "
+			_, ok := m.selected[i]
+			if ok || m.tasks[i][2] == "true" {
+				checked = "x"
+			}
+
+			s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, m.tasks[i][1])
+		}
+		return s
+	} else if m.mode == 1 {
+		s := "Type in the name of the task:\n\n" + m.input.View()
+		return s
+
+	} else {
+		s := "Select tasks for deleting:\n\n"
+
+		for i := range m.tasks {
+			cursor := " "
+			if m.cursor == i {
+				cursor = ">"
+			}
+
+			checked := " "
+			if _, ok := m.selected[i]; ok {
+				checked = "x"
+			}
+
+			s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, m.tasks[i][1])
+		}
+		return s
+	}
+}
+
 func main() {
-	fmt.Println(list())
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
