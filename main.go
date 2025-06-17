7d045fd1dd7ea9d9ef33e675f74844f8e468f883
youshitsune <youshitsune@dmz.rs>
2025-06-17 15:41:54 +0200

    simplify

--- main.go
+++ main.go
@@ -44,18 +44,17 @@ var cfg config
 
 type Task struct {
 	id     string
-	tag    string
 	title  string
 	status string
 }
 
-func (t Task) FilterValue() string { return t.title + t.tag }
+func (t Task) FilterValue() string { return t.title}
 func (t Task) Title() string       { return t.title }
 func (t Task) Description() string {
 	if t.status == "true" {
-		return t.tag + "\nFinished!"
+		return "\nFinished!"
 	}
-	return t.tag + "\nNot started!"
+	return "\nNot started!"
 }
 
 type Err struct {
@@ -79,7 +78,7 @@ func load_config() config {
 }
 
 func list_tasks() []list.Item {
-	res, err := fetch.Post(cfg.url+"/list", auth)
+	res, err := fetch.Get(cfg.url+"/list", auth)
 
 	if err != nil {
 		mainErr = err
@@ -89,30 +88,17 @@ func list_tasks() []list.Item {
 	t := strings.Split(string(res.Body), "\n")
 	for i := range t {
 		tmp := strings.Split(t[i], "``")
-		if len(tmp) == 4 {
-			r = append(r, Task{id: tmp[0], title: tmp[1], status: tmp[2], tag: tmp[3]})
+		if len(tmp) == 3 {
+			r = append(r, Task{id: tmp[0], title: tmp[1], status: tmp[2]})
 		}
 	}
 
 	return r
 }
 
-func get_note(id string) string {
-	auth.Query = map[string]string{"id": id}
-	res, err := fetch.Post(cfg.url+"/getnote", auth)
-
-	if err != nil {
-		mainErr = err
-		return ""
-	}
-
-	return string(res.Body)
-}
-
-func add(task, tag string) {
+func add(task string) {
 	auth.Query = map[string]string{
 		"task": task,
-		"tag":  tag,
 	}
 
 	res, err := fetch.Post(cfg.url+"/new", auth)
@@ -126,26 +112,9 @@ func add(task, tag string) {
 	}
 }
 
-func addnote(id, note string) {
-	auth.Query = map[string]string{
-		"id":   id,
-		"note": note,
-	}
-
-	res, err := fetch.Post(cfg.url+"/newnote", auth)
-
-	if err != nil {
-		mainErr = err
-	}
-
-	if res.StatusCode() != 200 {
-		mainErr = &Err{"Error while adding note"}
-	}
-}
-
-func done(id string) {
+func change(id string) {
 	auth.Query = map[string]string{"id": id}
-	res, err := fetch.Post(cfg.url+"/done", auth)
+	res, err := fetch.Post(cfg.url+"/change", auth)
 
 	if err != nil {
 		mainErr = err
@@ -156,18 +125,6 @@ func done(id string) {
 	}
 }
 
-func reset(id string) {
-	auth.Query = map[string]string{"id": id}
-	res, err := fetch.Post(cfg.url+"/reset", auth)
-
-	if err != nil {
-		mainErr = err
-	}
-
-	if res.StatusCode() != 200 {
-		mainErr = &Err{"Error while reseting task state"}
-	}
-}
 
 func deleteTask(id string) {
 	auth.Query = map[string]string{"id": id}
@@ -179,19 +136,6 @@ func deleteTask(id string) {
 
 	if res.StatusCode() != 200 {
 		mainErr = &Err{"Error while deleting task"}
-	}
-}
-
-func deleteNote(id string) {
-	auth.Query = map[string]string{"id": id}
-	res, err := fetch.Post(cfg.url+"/deletenote", auth)
-
-	if err != nil {
-		mainErr = err
-	}
-
-	if res.StatusCode() != 200 {
-		mainErr = &Err{"Error while deleting note"}
 	}
 }
 
@@ -208,22 +152,6 @@ func rename(id, task string) {
 
 	if res.StatusCode() != 200 {
 		mainErr = &Err{"Error while renaming task"}
-	}
-}
-
-func editTag(id, tag string) {
-	auth.Query = map[string]string{
-		"id":  id,
-		"tag": tag,
-	}
-	res, err := fetch.Post(cfg.url+"/edittag", auth)
-
-	if err != nil {
-		mainErr = err
-	}
-
-	if res.StatusCode() != 200 {
-		mainErr = &Err{"Error while editing tag"}
 	}
 }
 
@@ -288,12 +216,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			switch msg.String() {
 			case "enter":
 				id := m.tasks.SelectedItem().(Task).id
-				status := m.tasks.SelectedItem().(Task).status
-				if status == "true" {
-					reset(id)
-				} else {
-					done(id)
-				}
+				change(id)
 				cmd := m.tasks.SetItems(list_tasks())
 				cmds = append(cmds, cmd)
 			case "ctrl+c", "q":
@@ -310,43 +233,13 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				m.selected = m.tasks.SelectedItem().(Task)
 				m.mode = Rename
 				m.input.SetValue(m.selected.title)
-			case "n":
-				m.selected = m.tasks.SelectedItem().(Task)
-				note := get_note(m.selected.id)
-				if note == "" {
-					m.note.SetValue("")
-				} else {
-					m.note.SetValue(note)
-				}
-				m.mode = Note
-			case "t":
-				m.selected = m.tasks.SelectedItem().(Task)
-				m.mode = EditTag
-				m.input.SetValue(m.selected.tag)
 			}
-
 		} else if m.mode == Add {
 			var cmd tea.Cmd
 			switch msg.String() {
 			case "enter":
 				m.new = m.input.Value()
-				cmd = m.tasks.SetItems(list_tasks())
-				cmds = append(cmds, cmd)
-				m.mode = AddTag
-				m.input.SetValue("")
-			case "ctrl+c":
-				return m, tea.Quit
-			case "esc":
-				m.new = ""
-				m.mode = Home
-			}
-			m.input, cmd = m.input.Update(msg)
-			cmds = append(cmds, cmd)
-		} else if m.mode == AddTag {
-			var cmd tea.Cmd
-			switch msg.String() {
-			case "enter":
-				add(m.new, m.input.Value())
+				add(m.new)
 				cmd = m.tasks.SetItems(list_tasks())
 				cmds = append(cmds, cmd)
 				m.mode = Home
@@ -358,7 +251,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			}
 			m.input, cmd = m.input.Update(msg)
 			cmds = append(cmds, cmd)
-		} else if m.mode == Rename {
+		}  else if m.mode == Rename {
 			var cmd tea.Cmd
 			switch msg.String() {
 			case "enter":
@@ -374,39 +267,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			}
 			m.input, cmd = m.input.Update(msg)
 			cmds = append(cmds, cmd)
-		} else if m.mode == Note {
-			switch msg.String() {
-			case "ctrl+s":
-				addnote(m.selected.id, m.note.Value())
-				m.selected = Task{}
-				m.mode = Home
-			case "ctrl+c":
-				return m, tea.Quit
-			case "esc":
-				m.mode = Home
-			case "ctrl+d":
-				deleteNote(m.selected.id)
-				m.mode = Home
-			}
-			var cmd tea.Cmd
-			m.note, cmd = m.note.Update(msg)
-			cmds = append(cmds, cmd)
-		} else if m.mode == EditTag {
-			switch msg.String() {
-			case "enter":
-				editTag(m.selected.id, m.input.Value())
-				m.tasks.SetItems(list_tasks())
-				m.selected = Task{}
-				m.mode = Home
-			case "ctrl+c":
-				return m, tea.Quit
-			case "esc":
-				m.mode = Home
-			}
-			var cmd tea.Cmd
-			m.input, cmd = m.input.Update(msg)
-			cmds = append(cmds, cmd)
-		}
+		} 	
 	}
 	var cmd tea.Cmd
 	m.tasks, cmd = m.tasks.Update(msg)
@@ -417,15 +278,9 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 func (m model) View() string {
 	if m.mode == Add {
 		return style.Render("Name of the task:\n\n" + m.input.View())
-	} else if m.mode == AddTag {
-		return style.Render("Tag of the task:\n\n" + m.input.View())
 	} else if m.mode == Rename {
 		return style.Render("Rename the task:\n\n" + m.input.View())
-	} else if m.mode == Note {
-		return style.Render("Note of the task:\n\n" + m.note.View())
-	} else if m.mode == EditTag {
-		return style.Render("Edit tag of the task:\n\n" + m.input.View())
-	}
+	} 	
 	return styleTasks.Render(m.tasks.View())
 }
 
